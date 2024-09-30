23cc5e59c897fb3c7481196ac4ece3c264ba02e9
diogofragoso <diogofragoso@yahoo.com.br>
2024-09-30 17:11:45 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/example/bandodedados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bandodedados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/menu.dart
@@ -0,0 +1,25 @@
+class Menu {
+  static const perguntas = [
+    {
+      "Prato": "Dia a dia",
+      "Acompanhamentos": [
+        "Arroz Branco",
+        "Feijão Branco",
+        "Macarrão",
+        "Frango",
+      ],
+      
+    },
+
+    {
+      "Prato": "Speciale",
+      "Acompanhamentos": [
+        "Arroz Branco",
+        "Feijão Branco",
+        "Strogonoff",
+        "Bife",
+      ],
+      
+    },
+  ];
+}
